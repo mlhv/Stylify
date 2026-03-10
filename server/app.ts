6d3044eeea6f49a4cb7b51ff640bb8cffbead601
Minh Le <levuhoangminh1@gmail.com>
2026-03-10 11:22:46 -0400

    did bun init for backend files

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
-import { serveStatic } from 'hono/bun'
 import { wardrobeRoute } from './routes/wardrobe'
 import { authRoute } from './routes/auth'
 import { signedUrlRoute } from './routes/signedUrl'
@@ -9,12 +8,10 @@ const app = new Hono()
 
 app.use(logger())
 
-const apiRoutes = app.basePath('/api').route('/wardrobe', wardrobeRoute).route('/', authRoute).route('/signed-url', signedUrlRoute)
-
-// do bun run build to generate frontend/dist
-
-app.get('*', serveStatic({ root: './frontend/dist' }))
-app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
+const apiRoutes = app.basePath('/api')
+    .route('/wardrobe', wardrobeRoute)
+    .route('/', authRoute)
+    .route('/signed-url', signedUrlRoute)
 
 export default app
 export type ApiRoutes = typeof apiRoutes
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,6 @@
 import app from './app';
-import { config } from 'dotenv';
 
-config();
-
-const server = Bun.serve({
-    fetch: app.fetch,
-    port: process.env.PORT || 3000,
-})
-
-console.log("server running", server.port);
+export default {
+  port: 8080,
+  fetch: app.fetch,
+}
