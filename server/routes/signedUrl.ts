ed430158d0f67637aeafa74ab4bed0bf98ab29f2
Minh Le <levuhoangminh1@gmail.com>
2026-04-25 14:29:18 -0400

    removed credentials from s3 client, we get it from lambda

--- server/routes/signedUrl.ts
+++ server/routes/signedUrl.ts
@@ -5,10 +5,6 @@ import { getUser } from "../kinde";
 
 const s3 = new S3Client({
     region: process.env.AWS_BUCKET_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-    }
 });
 
 const maxFileSize = 1024 * 1024 * 10; // 10 MB
